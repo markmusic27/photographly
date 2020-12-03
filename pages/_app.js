89c10c56b6b6c23463f00375c29820f1b1fbcb8c
Mark Music <markmusic999@gmailcom>
2020-12-03 11:06:33 -0600

    fix(general): 🦑 Finish Setup of App

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
+import { BackgroundImage } from "../pages/index"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <BackgroundImage />
 }
 
 export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import react from "react";
+
+export class BackgroundImage extends react.Component {
+    render(){
+        return <div>Hello World</div>
+    }
+}
